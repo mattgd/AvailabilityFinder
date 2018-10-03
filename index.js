e98ea79f2695f4f53302690a4c4a3f8b1a8d35c2
Matt Dzwonczyk <9063128+mattgd@users.noreply.github.com>
2018-10-02 23:19:21 -0400

    Buffer time functionality and conversion to CLI (#11)
    * Add basic buffer time functionality.
    
    * Resolve #10 converting to a CLI, and resolve #9.
    
    * Refactor Date manipulation functions.
    
    * Update Travis config.
    
    * Update code coverage to use nyc.
    
    * Add badges to README

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-const AvailabilitySlot = require('./AvailabilitySlot');
+#!/usr/bin/env node
+'use strict';
 
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-const moment = require('moment');
+const AvailabilitySlot = require('./AvailabilitySlot'),
+      { convertToDate, addTimeString } = require('./DateUtil'),
+      fs = require('fs'),
+      {google} = require('googleapis'),
+      moment = require('moment'),
+      program = require('commander'),
+      readline = require('readline');
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'token.json';
 const DEFAULT_DATE_FORMAT = 'ddd MM/DD h:mm a';
+const DEFAULT_BUFFER_TIME = 30;
+const MS_PER_MINUTE = 60000;
 
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), findAvailability);
-});
+program
+  .version('0.0.2')
+  .description('Finds and lists out availability from your Google Calendar.')
+  .option('-S, --start-date <date>', 'availability search start date')
+  .option('-E, --end-date <date>', 'availability search end date')
+  .option('-d, --date-format <format>', 'optional output date format')
+  .option('-n, --now [range]', 'set availability start/end dates based on ' +
+    'current date/time where range is days or weeks (e.g. 5d, 1w), default 1w')
+  .option('-b, --buffer-time <time>', 'amount of buffer time between events and availability')
+  .action(run)
+  .parse(process.argv); // end with parse to parse through the input
+
+/**
+ * Initiates the AvailabilityFinder program.
+ */
+function run() {
+  if (!(program.startDate && program.endDate) && !program.now) {
+    // If program was called with no start/end date, show help.
+    program.help();
+  } else {
+    const startDate = program.startDate ? convertToDate(program.startDate) : new Date();
+    const endDate = program.endDate ? convertToDate(program.endDate) : addTimeString(startDate, program.now);
+
+    // Load client secrets from a local file.
+    fs.readFile('credentials.json', (err, content) => {
+      if (err) {
+        return console.log('Error loading client secret file:', err);
+      }
+
+      // Authorize a client with credentials, then call the Google Calendar API.
+      authorize(JSON.parse(content), findAvailability, startDate, endDate);
+    });
+  }
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -22,16 +56,19 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(credentials, callback, startDate, endDate) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
+    if (err) {
+      return getAccessToken(oAuth2Client, callback, startDate, endDate);
+    }
+
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    callback(oAuth2Client, startDate, endDate);
   });
 }
 
@@ -40,13 +77,16 @@ function authorize(credentials, callback) {
  * execute the given callback with the authorized OAuth2 client.
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
+ * @param {Date} startDate The startDate to search from.
+ * @param {Date} endDate The startDate to search to.
  */
-function getAccessToken(oAuth2Client, callback) {
+function getAccessToken(oAuth2Client, callback, startDate, endDate) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
   console.log('Authorize this app by visiting this url:', authUrl);
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -54,14 +94,22 @@ function getAccessToken(oAuth2Client, callback) {
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) {
+        return console.error('Error retrieving access token', err);
+      }
+
       oAuth2Client.setCredentials(token);
+
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
+        if (err) {
+          console.error(err);
+        }
+
         console.log('Token stored to', TOKEN_PATH);
       });
-      callback(oAuth2Client);
+
+      callback(oAuth2Client, startDate, endDate);
     });
   });
 }
@@ -70,97 +118,71 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function findAvailability(auth) {
+function findAvailability(auth, startDate, endDate) {
   const calendar = google.calendar({version: 'v3', auth});
 
-  getDateInput('start date', function(startDate) {
-    if (startDate) {
-      getDateInput('end date', function(endDate) {
-        if (endDate) {
-          calendar.events.list({
-            calendarId: 'primary',
-            timeMin: startDate,
-            timeMax: endDate,
-            singleEvents: true,
-            orderBy: 'startTime',
-          }, (err, res) => {
-            if (err) {
-              return console.log('The API returned an error: ' + err);
-            }
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: startDate,
+    timeMax: endDate,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) {
+      return console.log('The API returned an error: ' + err);
+    }
 
-            const events = res.data.items;
-            if (events.length) {
-              var availableSlots = getAvailabilitySlots(startDate, events);
+    const events = res.data.items;
+    if (events.length) {
+      var bufferTime = program.bufferTime ? +program.bufferTime : DEFAULT_BUFFER_TIME;
+      var availableSlots = getAvailabilitySlots(startDate, endDate, bufferTime, events);
 
-              const rl = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout,
-              });
-
-              // Print out the header
-              rl.write(`Availability from ${moment(startDate).format(DEFAULT_DATE_FORMAT)} to ${moment(endDate).format(DEFAULT_DATE_FORMAT)}:\n`);
-
-              // Print out the availability slot
-              availableSlots.map((slot) => {
-                rl.write(`\u2022 ${slot}\n`);
-              });
-
-              rl.close();
-              process.stdin.destroy();
-            } else {
-              console.log('No upcoming events found.');
-            }
-          });
-        }
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
       });
+
+      // Print out the header
+      rl.write(`Availability from ${moment(startDate).format(DEFAULT_DATE_FORMAT)} to ${moment(endDate).format(DEFAULT_DATE_FORMAT)}:\n`);
+
+      // Print out the availability slot
+      availableSlots.map((slot) => {
+        rl.write(`\u2022 ${slot}\n`);
+      });
+
+      rl.close();
+      process.stdin.destroy();
+    } else {
+      console.log('No upcoming events found.');
     }
   });
 }
 
 /**
  * Returns an Array of AvailablitySlot objects.
+ * @param {Date} startDate The date to start looking for availability.
+ * @param {Date} endDate The date to stop looking for availability.
+ * @param {Date} bufferTime The amount of time buffer events with.
  * @param {object[]} events An Array of Google Calendar event objects.
  * @return {AvailabilitySlot[]} An Array of AvailablitySlot objects.
  */
-function getAvailabilitySlots(startDate, events) {
+function getAvailabilitySlots(startDate, endDate, bufferTime, events) {
   var availabilitySlots = [];
   var slotStartDate = startDate;
+  var event;
 
   for (event of events) {
     const start = event.start.dateTime || event.start.date;
     const end = event.end.dateTime || event.end.date;
 
-    var slot = new AvailabilitySlot(slotStartDate, start);                
+    var slot = new AvailabilitySlot(slotStartDate, moment(start).subtract(bufferTime, 'm').toDate());                
     availabilitySlots.push(slot);
 
     slotStartDate = end; // Set to end of current event
   }
 
+  // Add any remaining availability after events.
+  availabilitySlots.push(new AvailabilitySlot(slotStartDate, endDate));
+
   return availabilitySlots;
-}
-
-/**
- * Gets a date input from stdin by prompting the user.
- * @param {string} dateName Name of the date for output purposes.
- * @param {function} callback The result callback.
- */
-function getDateInput(dateName, callback) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.question('Enter the ' + dateName + ' for availability: ', (dateStr) => {
-    let date = new Date(dateStr);
-
-    // Check for invalid dateStr
-    if (!date) {
-      rl.write(dateStr + ' is not in a valid date format.');
-      rl.close();
-      callback(null);
-    }
-
-    rl.close();
-    callback(date.toISOString());
-  });
 }
---
+++ test/DateUtil.test.js
@@ -0,0 +1,44 @@
+const expect = require('chai').expect,
+      { convertToDate, addTimeString } = require('../DateUtil'),
+      moment = require('moment');
+
+describe('convertToDate()', function () {
+  it('should return valid JavaScript Date object', function () {
+    // 1. ARRANGE
+    const expected = new Date(2018, 9, 4);
+
+    const dateStr = '10-04-2018';
+
+    // 2. ACT
+    var actual = convertToDate(dateStr);
+
+    // 3. ASSERT
+    expect(actual.valueOf()).to.be.equal(expected.valueOf());
+  });
+
+  it('should throw an error', function () {
+    // 1. ARRANGE
+    const expected = new Date(2018, 9, 4);
+
+    const dateStr = '10-Inv4lid';
+
+    // 2 & 3. ACT & ASSERT
+    expect(() => convertToDate(dateStr)).to.throw();
+  });
+});
+
+describe('addTimeString()', function () {
+  it('should return the current date plus one week', function () {
+    // 1. ARRANGE
+    const now = new Date();
+    const expected = moment(now).add(1, 'w').toDate();
+
+    const timeStr = '1w';
+
+    // 2. ACT
+    var actual = addTimeString(now, timeStr);
+
+    // 3. ASSERT
+    expect(actual.valueOf()).to.be.equal(expected.valueOf());
+  });
+});
